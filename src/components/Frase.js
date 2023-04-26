fdf12482fc7398c7b381b3555742a3d3a68174a9
TayEsp <tayna.respinosa@gmail.com>
2023-04-26 13:48:30 -0300

    Aprendendo sobre componentes

---
+++ src/components/Frase.js
@@ -0,0 +1,12 @@
+function Frase(){
+    return(
+        <div>
+            <p>----------------------------------------</p>
+            <p>este é um componente que divide</p>
+            <p>----------------------------------------</p>
+        </div>
+    )
+
+}
+
+export default Frase
